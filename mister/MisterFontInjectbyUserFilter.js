27632467e8aa377beef8c28aefd77453a6381ebb
kalebbadger <113721322+kalebbadger@users.noreply.github.com>
2023-04-12 14:59:13 -0700

    Update MisterFontInjectbyUserFilter.js

--- mister/MisterFontInjectbyUserFilter.js
+++ mister/MisterFontInjectbyUserFilter.js
@@ -14,7 +14,7 @@ googlelink2.href = 'https://fonts.gstatic.com';
 googlelink2.rel = 'preconnect';
 
 const fontlink = document.createElement('link');
-fontlink.href = 'https://fonts.googleapis.com/css2?family=Arvo:wght@700&family=Figtree:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap';
+fontlink.href = 'https://fonts.googleapis.com/css2?family=HARPO:wght@700&family=Figtree:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap';
 fontlink.rel = 'stylesheet';
 
 
