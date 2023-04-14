fe16ae702107f6290ab6b8a318ac9d2172d67e3e
kalebbadger <113721322+kalebbadger@users.noreply.github.com>
2023-04-14 13:19:20 -0700

    Update westpress-css-filter.js

--- westpress/javascript/westpress-css-filter.js
+++ westpress/javascript/westpress-css-filter.js
@@ -4,7 +4,7 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 
 //Vantage West
 if (z.includes("Vantage")){
-document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/VWCU_Catalog.css?v=4";}
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://kalebbadger.github.io/marcom/westpress/css/vwcu-catalog.css";}
 
 //Pima Federal
 else if (z.includes("Pima Federal")){
