0042ccb7ca908b8a75b576f7f576f06577b9e13a
kalebbadger <113721322+kalebbadger@users.noreply.github.com>
2023-04-14 08:22:20 -0700

    Update westpress-css-filter.js

--- westpress/javascript/westpress-css-filter.js
+++ westpress/javascript/westpress-css-filter.js
@@ -4,26 +4,26 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 
 //Vantage West
 if (z.includes("Vantage")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPress/CSS/VWCU_Catalog.css?v=4";}
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/VWCU_Catalog.css?v=4";}
 
 //Pima Federal
 else if (z.includes("Pima Federal")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPress/CSS/PFCUs_Catalog.css?v=1";}
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/PFCUs_Catalog.css?v=1";}
 
 //Sierra Tucson
 else if (z.includes("Sierra Tucson")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPress/CSS/SIET_Catalog.css?v=1";}
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/SIET_Catalog.css?v=1";}
 
 //Cochise College
 else if (z.includes("Cochise College")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPress/CSS/CochiseCollege_Catalog.css?v=1";}
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/CochiseCollege_Catalog.css?v=1";}
 
 //Cochise County
 else if (z.includes("Cochise County")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPress/CSS/CochiseCounty_Catalog.css?v=2";
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPress/CSS/CochiseCounty_Catalog.css?v=2";
 }
 
 //Senior Resource Group
 else if (z.includes("Senior Resource Group")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/WestPress/CSS/WestPress_CSSBase.css?v=5']").href = "https://westpress.com/MarcomContent/WestPressMarketingPromo/CSS/SRG_Catalog.css?v=39";
+document.querySelector("link[href='https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPressMarketingPromo/CSS/SRG_Catalog.css?v=39";
 }
