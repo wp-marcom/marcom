df78ddfb6b2a365cd12b6d91469d975a34f9fa2a
kalebbadger <113721322+kalebbadger@users.noreply.github.com>
2023-04-18 14:36:53 -0700

    Update westpress-injectcss.js

--- westpress/javascript/westpress-injectcss.js
+++ westpress/javascript/westpress-injectcss.js
@@ -1,1 +1,8 @@
-document.querySelector("#Defaultstylesheet").removeAttribute('disabled');
+var y = window.userId
+var z = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+
+//Vantage West
+if (z.includes("Vantage")){
+document.querySelector("#vantage_stylesheet").removeAttribute('disabled');
+}
