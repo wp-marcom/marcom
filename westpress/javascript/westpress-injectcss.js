5ce61d4d80d4198544f21fc4b58fc1bf83449ed8
kalebbadger <113721322+kalebbadger@users.noreply.github.com>
2023-04-18 14:29:44 -0700

    Update westpress-injectcss.js

--- westpress/javascript/westpress-injectcss.js
+++ westpress/javascript/westpress-injectcss.js
@@ -1,7 +1,1 @@
-//Load Custom CSS in HEAD
-
-const csslink = document.createElement('link');
-csslink.href = 'https://kalebbadger.github.io/marcom/westpress/css/westpress-cssbase.css';
-csslink.rel = 'stylesheet';
-
-document.getElementsByTagName('head')[0].appendChild(csslink);
+document.querySelector("#Defaultstylesheet").removeAttribute('disabled');
