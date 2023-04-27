d864167a24b6fe900d4f90b671e38a886b81f79f
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-04-27 16:40:54 -0700

    Update westpress-css-filter.js

--- westpress/javascript/westpress-css-filter.js
+++ westpress/javascript/westpress-css-filter.js
@@ -4,7 +4,7 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 
 //Vantage West
 if (z.includes("Vantage")){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://wp-marcom.github.io/marcom/westpress/css/vwcu-catalog.css";}
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://wp-marcom.github.io/marcom/westpress/css/westpress-cssbase.css";}
 
 //Pima Federal
 else if (z.includes("Pima Federal")){
