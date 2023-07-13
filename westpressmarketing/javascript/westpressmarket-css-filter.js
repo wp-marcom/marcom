1881494d96b0931a5d26ec6b55bc929bbebf2b98
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-07-13 10:00:22 -0700

    Update westpressmarket-css-filter.js

--- westpressmarketing/javascript/westpressmarket-css-filter.js
+++ westpressmarketing/javascript/westpressmarket-css-filter.js
@@ -4,5 +4,5 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 
 //Senior Resource Group
 if (z.includes("Senior Resource Group")){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/srg-catalog.css";
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpressmarketing/css/westpressmarket_catalog_base.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/srg-catalog.css";
 }
