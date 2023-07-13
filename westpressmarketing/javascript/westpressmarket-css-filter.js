c08d90a1afb3b2c33eb049d5cc2efeb024918a65
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-07-13 09:16:40 -0700

    Add files via upload

---
+++ westpressmarketing/javascript/westpressmarket-css-filter.js
@@ -0,0 +1,8 @@
+var y = window.userId
+var z = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+
+//Senior Resource Group
+if (z.includes("Senior Resource Group")){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress-cssbase.css']").href = "https://westpress.com/MarcomContent/WestPressMarketingPromo/CSS/SRG_Catalog.css?v=39";
+}
