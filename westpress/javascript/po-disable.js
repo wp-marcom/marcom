9b95f5d06b91e25bdd29e4ebc0a9422209adceec
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-04 07:30:03 -0700

    Add files via upload

---
+++ westpress/javascript/po-disable.js
@@ -0,0 +1,3 @@
+document.getElementById("ctl00_content_CartBilling_divClientPONumberDisplay").required = false;
+
+document.getElementById("ctl00_content_CartBilling_txtClientPONumber").required = false;
