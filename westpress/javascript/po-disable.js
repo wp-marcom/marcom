5af87bd8eaae261479ec4dacfd5fcbdabb931858
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-04 07:46:12 -0700

    Update po-disable.js

--- westpress/javascript/po-disable.js
+++ westpress/javascript/po-disable.js
@@ -2,7 +2,7 @@ document.getElementById("ctl00_content_CartBilling_divClientPONumberDisplay").re
 
 document.getElementById("ctl00_content_CartBilling_txtClientPONumber").required = false;
 
-document.getElementById("ctl00_content_CartBilling_CtlCartItemList_CtlCartItems_ctl02_txtItemClientPONumber).required = false;
+document.getElementById("ctl00_content_CartBilling_CtlCartItemList_CtlCartItems_ctl02_txtItemClientPONumber").required = false;
 
 document.getElementById("ctl00_content_CartBilling_divClientPONumberDisplay").removeAttribute("required");
 
