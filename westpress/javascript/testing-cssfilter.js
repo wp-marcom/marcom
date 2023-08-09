e76af8e18edcbca0f58e1aa22e9f41329aff0b74
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-09 12:20:00 -0700

    Create testing-cssfilter.js

---
+++ westpress/javascript/testing-cssfilter.js
@@ -0,0 +1,8 @@
+var y = window.userId
+var z = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+
+//Mister
+if (z.includes("Badger")){
+document.querySelector("link[href='https://westpress.com/MarcomContent/MCW/CSS/MCW_Catalog_v13.css?v=2']").href = "https://wp-marcom.github.io/marcom/mister/css/mister_catalog.css";}
+}
