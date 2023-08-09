fdcd98e239b471246554b8a3e05a0aa1191c134f
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-09 12:20:33 -0700

    Delete testing-css-filter.js

--- westpress/javascript/testing-css-filter.js
+++
@@ -1,8 +0,0 @@
-var y = window.userId
-var z = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
-var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
-
-//Testing
-if (z.includes("Kaleb Badger")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/UofA/CSS/UA_Catalog-v22.css']").href = "https://wp-marcom.github.io/marcom/uofa/css/uofa-cssbase.css";}
-
