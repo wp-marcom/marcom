da0f531dc54cabe75cd598ee56dc54e2ecfa7685
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-09 12:39:01 -0700

    Update testing-cssfilter.js

--- westpress/javascript/testing-cssfilter.js
+++ westpress/javascript/testing-cssfilter.js
@@ -5,4 +5,3 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 //Mister
 if (z.includes("Kaleb Badger")){
 document.querySelector("link[href='https://westpress.com/MarcomContent/MCW/CSS/MCW_Catalog_v13.css?v=2']").href = "https://wp-marcom.github.io/marcom/mister/css/mister_catalog.css";}
-}
