1ed1f8b5ed1485d26d0ededeb04e2ce5bb1148fc
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-11 12:47:07 -0700

    Update testing-cssfilter.js

--- westpress/javascript/testing-cssfilter.js
+++ westpress/javascript/testing-cssfilter.js
@@ -6,8 +6,7 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML;
 if (z.includes("Kaleb Badger")){
 document.querySelector("link[href='https://westpress.com/MarcomContent/MCW/CSS/MCW_Catalog_v13.css?v=2']").href = "https://wp-marcom.github.io/marcom/mister/css/mister_catalog.css";
   
-let a = document.getElementsByClassName( "gridRow3Cols" );
-[...a].forEach( x => x.className += " container-fluid" );
-[...a].forEach( x => x.classList.remove("gridRow3Cols") );
+const boxes = document.getElementsByTagName("div");
+boxes[0].classList.remove("gridRow3Cols");
   
 }
