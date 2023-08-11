350e0cad22623ec159292418221af5d4c2250ed8
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-11 12:21:41 -0700

    Update testing-cssfilter.js

--- westpress/javascript/testing-cssfilter.js
+++ westpress/javascript/testing-cssfilter.js
@@ -4,4 +4,10 @@ var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML;
 
 //Mister
 if (z.includes("Kaleb Badger")){
-document.querySelector("link[href='https://westpress.com/MarcomContent/MCW/CSS/MCW_Catalog_v13.css?v=2']").href = "https://wp-marcom.github.io/marcom/mister/css/mister_catalog.css";}
+document.querySelector("link[href='https://westpress.com/MarcomContent/MCW/CSS/MCW_Catalog_v13.css?v=2']").href = "https://wp-marcom.github.io/marcom/mister/css/mister_catalog.css";
+  
+let a = document.getElementsByClassName( "gridRow3Cols" );
+[...a].forEach( x => x.className += " container-fluid" );
+[...a].forEach( x => x.classList.remove("gridRow3Cols") );
+  
+}
