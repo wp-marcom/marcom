fec06a12a3a227d85f8b5c8c91728c0492e21e93
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-23 12:58:41 -0700

    Update lottie-web-2.js

--- westpress/javascript/lottie-web-2.js
+++ westpress/javascript/lottie-web-2.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('LogoDiv'),
-  path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+  path: 'https://lottie.host/d6baa922-327a-4396-9063-1f101457e9ef/7cBrRDdq3t.json',
   renderer: 'svg',
   loop: true,
   autoplay: true,
