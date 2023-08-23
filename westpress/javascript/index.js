95f23e3c6b99f0bd9dd00f1438c725c9cf933921
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-23 10:44:05 -0700

    Add files via upload

---
+++ westpress/javascript/index.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json'
+  })
