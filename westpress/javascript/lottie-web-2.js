2ac21296249de05d1ad53c1d3669890f9869ecc1
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-23 12:44:03 -0700

    Create lottie-web-2.js

---
+++ westpress/javascript/lottie-web-2.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('container'),
+  path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  name: "Demo Animation",
+});
