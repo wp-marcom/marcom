4573451a9ebc10f12f113db7fe1e03d4058a58d8
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-29 13:12:12 -0700

    Update lottie-web-2.js

--- westpress/javascript/lottie-web-2.js
+++ westpress/javascript/lottie-web-2.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
   container: document.querySelector(".NavTop"),  
-  path: 'https://lottie.host/83fcc8b0-83d8-4cf4-b5df-5a27b993680c/AOhKp3Pddg.json',
+  path: 'https://lottie.host/c62de71e-a406-4792-abc9-35020a4cc93f/48gdaeRNXI.json',
   renderer: 'svg',
   loop: true,
   autoplay: true,
