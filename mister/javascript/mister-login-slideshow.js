a32e6af4b2649d0ed1d8ae7d192bc8abdf94e768
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-30 14:42:36 -0700

    Update mister-login-slideshow.js

--- mister/javascript/mister-login-slideshow.js
+++ mister/javascript/mister-login-slideshow.js
@@ -7,19 +7,19 @@ var randomNum = getRandomInt(6);
 
 //Vantage West
 if (randomNum==0){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp";}
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp)";}
 
 else if (randomNum==1){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp)";}
 
 else if (randomNum==2){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp)";}
 
 else if (randomNum==3){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp)";}
 
 else if (randomNum==4){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
-
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp)";}
+  
 else if (randomNum==5){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+document.getElementById('ENUSmain').style.backgroundImage="url(https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp)";}
