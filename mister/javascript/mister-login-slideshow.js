f6b3ecc4ff603a6118a41fab4f7f8cbc86ecd3fa
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-30 14:29:10 -0700

    Update mister-login-slideshow.js

--- mister/javascript/mister-login-slideshow.js
+++ mister/javascript/mister-login-slideshow.js
@@ -2,4 +2,24 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-console.log(getRandomInt(6));
+var randomNum = getRandomInt(6);
+
+
+//Vantage West
+if (randomNum==0){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp";}
+
+else if (randomNum==1){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+
+else if (randomNum==2){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+
+else if (randomNum==3){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+
+else if (randomNum==4){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
+
+else if (randomNum==5){
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/mister/images/Product-HotShine-bw.webp']").href = "https://wp-marcom.github.io/marcom/mister/images/Platinum-Seal-Desktop-bw.webp";}
