2dc8699de427d875642d3042cdcb1d71275e5f81
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-30 12:53:03 -0700

    Update lottie-web-2.js

--- westpress/javascript/lottie-web-2.js
+++ westpress/javascript/lottie-web-2.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
   container: document.querySelector(".NavTop"),  
-  path: 'https://wp-marcom.github.io/marcom/mister/javascript/mister-header-spraydrive.json',
+  path: 'https://raw.githubusercontent.com/wp-marcom/marcom/main/mister/javascript/mister-header-spraydrive.json',
   renderer: 'svg',
   loop: true,
   autoplay: true,
