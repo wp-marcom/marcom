67aaac44eb8c7f55e6028aba215d35e22765ba30
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-30 14:04:21 -0700

    Update mister-header-insert.js

--- mister/javascript/mister-header-insert.js
+++ mister/javascript/mister-header-insert.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
   container: document.querySelector(".NavTop"),  
-  path: 'https://wp-marcom.github.io/marcom/mister/javascript/mister-header-spraydrive.json',
+  path: 'https://lottie.host/60b699b0-522c-43df-9f30-60265d9d9f95/3ReP1UHbAU.json',
   renderer: 'svg',
   loop: true,
   autoplay: true,
