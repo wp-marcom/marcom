def84fe0e36c81c41fb7d3d2c81515165fd98cc6
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-08-30 11:25:40 -0700

    Create login-slideshow.js

---
+++ westpress/javascript/login-slideshow.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.querySelector("#ENUSmain"),  
+  path: 'https://lottie.host/7d65b150-33ef-43dd-8c89-8c2479b1187c/teZvzw12fP.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  name: "Demo Animation",
+});
