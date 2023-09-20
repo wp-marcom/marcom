631f47782ef7fe8d9641c4b828cc7b83292685b3
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-09-20 06:51:58 -0700

    Add files via upload

---
+++ mister/javascript/mister-ship-popup.js
@@ -0,0 +1,19 @@
+
+// Get the modal
+var modal = document.getElementById('popup1');
+
+var span = document.getElementsByClassName("close")[0];
+
+window.onload = function () {
+     document.getElementById('popup1').style.visibility = "visible";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById('popup1').style.visibility = "hidden";
+  }
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
