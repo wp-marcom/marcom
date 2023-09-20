0ff39a248be3b9c8c799f8add161863480725d98
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-09-20 07:25:42 -0700

    Update mister-ship-popup.js

--- mister/javascript/mister-ship-popup.js
+++ mister/javascript/mister-ship-popup.js
@@ -4,9 +4,14 @@ var modal = document.getElementById('popup1');
 
 var span = document.getElementsByClassName("close")[0];
 
-window.onload = function () {
+
+setTimeout(function () {
      document.getElementById('popup1').style.visibility = "visible";
-};
+         },10000);
+
+// window.onload = function () {
+//     document.getElementById('popup1').style.visibility = "visible";
+//};
 
 window.onclick = function(event) {
   if (event.target == modal) {
