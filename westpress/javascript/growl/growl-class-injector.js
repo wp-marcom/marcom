b74390e799f2c27e6925f742c730fc1ba53c323b
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-10-10 08:47:51 -0700

    Update growl-class-injector.js

--- westpress/javascript/growl/growl-class-injector.js
+++ westpress/javascript/growl/growl-class-injector.js
@@ -1,3 +1,6 @@
 const span = document.querySelector("span");
 const classes = span.classList;
 classes.add("injectclass");
+
+let v = document.getElementById("growlNotif");
+            v.className += "addCSS";
