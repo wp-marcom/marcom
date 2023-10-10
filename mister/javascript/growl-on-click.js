54b2c720784ddbf163a268ce16f62e2134852361
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-10-10 11:27:43 -0700

    Update growl-on-click.js

--- mister/javascript/growl-on-click.js
+++ mister/javascript/growl-on-click.js
@@ -1,39 +1,8 @@
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
 
-// Get the modal
-
-// Get the modal  -the grey shadowy box in the BG
-var modal = document.getElementById('growlPopup1');
-
-var closeButt = document.getElementsByClassName("growl-notification__close")[0];
-var okayButt = document.getElementsByClassName("growl-notification__button")[0];
-
-var queuePop = document.querySelectorAll('[threedots="Marketing"]')[0];
-
-document.querySelectorAll('[threedots="Marketing"]')[0].addEventListener("click", myFunction);
-
-function myFunction() {
-  document.querySelectorAll('[threedots="Marketing"]')[0].innerText = "YOU CLICKED ME!";
-}
-
-
-//window.onload = function () {
-//     document.getElementById('popup1').style.visibility = "visible";
-//};
-
-queuePop.onclick = function() {
-  document.getElementById('growlPopup1').style.visibility = "visible";
-}
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    document.getElementById('growlPopup1').style.visibility = "hidden";
-  }
-}
-closeButt.onclick = function() {
-  modal.style.display = "none";
-}
-okayButt.onclick = function() {
-  modal.style.display = "none";
-}
+document.getElementById("ctl00_content_CtlCatalogGrid_CtlCatalogItemRepeater_ctl00_CatalogGridRow_divGridRow").appendChild(div);
