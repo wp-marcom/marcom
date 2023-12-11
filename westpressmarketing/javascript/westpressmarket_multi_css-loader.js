2a54230070564347a5530bcc64d9321bb21c172d
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-12-11 11:20:22 -0700

    Add files via upload

---
+++ westpressmarketing/javascript/westpressmarket_multi_css-loader.js
@@ -0,0 +1,7 @@
+var userName = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
+
+//Senior Resource Group
+if (userName.includes("Senior Resource Group")){
+  
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress_multi_custom.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/srg-catalog.css";
+}
