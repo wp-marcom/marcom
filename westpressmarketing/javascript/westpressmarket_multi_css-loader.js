2ba956f977e33b663ec5d2302d325c139f87a7a8
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-12-11 14:20:26 -0700

    Update westpressmarket_multi_css-loader.js

--- westpressmarketing/javascript/westpressmarket_multi_css-loader.js
+++ westpressmarketing/javascript/westpressmarket_multi_css-loader.js
@@ -7,7 +7,7 @@ document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/
 }
 
 //OneAZ Credit Union
-if (userName.includes("OneAZ Credit Union")){
+else if (userName.includes("OneAZ Credit Union")){
   
 document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress_multi_custom.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/one_az_multi_custom.css";
 }
