ab8e836604369c2acca7561f426a3f73b56e4ae7
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-12-12 11:30:01 -0700

    Update westpressmarket_multi_css-loader.js

--- westpressmarketing/javascript/westpressmarket_multi_css-loader.js
+++ westpressmarketing/javascript/westpressmarket_multi_css-loader.js
@@ -3,7 +3,7 @@ var userName = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
 //Senior Resource Group
 if (userName.includes("Senior Resource Group")){
   
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress_multi_custom.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/srg_multi_custom.css";
+document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpress/css/westpress_multi_custom.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/srg/css/srg_multi_custom.css";
 }
 
 //OneAZ Credit Union
