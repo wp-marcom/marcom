581a918843dcd01a17c736f7f7a829d18659fac0
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2023-12-15 09:15:54 -0700

    Delete westpressmarketing/javascript/westpressmarket-css-filter.js

--- westpressmarketing/javascript/westpressmarket-css-filter.js
+++
@@ -1,8 +0,0 @@
-var y = window.userId
-var z = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
-var x = document.getElementById("ctl00_ctl08_lblUserName").innerHTML
-
-//Senior Resource Group
-if (z.includes("Senior Resource Group")){
-document.querySelector("link[href='https://wp-marcom.github.io/marcom/westpressmarketing/css/westpressmarket_catalog_base.css']").href = "https://wp-marcom.github.io/marcom/westpressmarketing/css/srg-catalog.css";
-}
