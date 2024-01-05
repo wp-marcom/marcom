90c148ea06f60a68bc9e6b6a01dad8ba76dd1624
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2024-01-05 12:25:06 -0700

    Create growl-functions.js

---
+++ westpress/javascript/growl/growl-functions.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal = document.getElementById('popupContainer');
+
+var span = document.getElementsByClassName("growl-notification__close-icon")[0];
+
+window.onload = function () {
+     document.getElementById('popupContainer').style.visibility = "visible";
+
+};
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById('popupContainer').style.visibility = "hidden";
+  }
+}
+span.onclick = function() {
+  modal.style.display = "none";
