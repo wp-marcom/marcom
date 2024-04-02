15a428e6b71950268548c6fb26b0097a457469d1
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2024-04-02 12:56:48 -0700

    Update showData_westpressinventory.js

--- inventorytrackers/js/showData_westpressinventory.js
+++ inventorytrackers/js/showData_westpressinventory.js
@@ -1,111 +1,108 @@
-// showData.js
-
 function showData(dataArray) {
-  // Fetch the JSON data asynchronously
-  async function fetchJsonData() {
-    var jsonDataUrl = 'https://wp-marcom.github.io/marcom/inventorytrackers/json/westpressinventory-producthumbsource.json';
-    try {
-      var response = await fetch(jsonDataUrl);
-      var jsonData = await response.json();
-      return jsonData;
-    } catch (error) {
-      console.error('Error fetching JSON data:', error);
-      throw error;
+    // Fetch the JSON data asynchronously
+    async function fetchJsonData() {
+        var jsonDataUrl = 'https://wp-marcom.github.io/marcom/inventorytrackers/json/westpressinventory-producthumbsource.json';
+        try {
+            var response = await fetch(jsonDataUrl);
+            var jsonData = await response.json();
+            return jsonData;
+        } catch (error) {
+            console.error('Error fetching JSON data:', error);
+            throw error;
+        }
     }
-  }
 
-  // Render the DataTable after fetching JSON data
-  fetchJsonData().then(jsonData => {
-    var table = $('#data-table').DataTable({
-      data: dataArray,
-      columns: [
-        {"title": "Product Name"}, // Disable sorting for the first column
-        {"title": "SKU"},
-        {"title": "Location"},
-        {"title": "Unit"},
-        {"title": "Unit Qty"},
-        {"title": "Min Qty"},
-        {"title": "Refill Qty"},
-        {"title": "QOS"},
-        {"title": "Status"},
-        {"title": "Notes"}
-      ],
-      order: [], // Disable initial sorting
-      pageLength: 100, // Set default number of entries per page to 100
-      columnDefs: [
-        {targets: [0, 1, 2, 3, 4, 5, 6, 7, 8], className: 'text-nowrap'}, // Apply 'text-nowrap' class to columns 0 to 8
-        {
-          targets: 0, // Target the Product Name column
-render: function (data, type, row, meta) {
-    if (type === 'display') {
-        var originalProductName = data;
-        var productName = originalProductName;
+    // Render the DataTable after fetching JSON data
+    fetchJsonData().then(jsonData => {
+        var table = $('#data-table').DataTable({
+            data: dataArray,
+            columns: [
+                { "title": "Product Name" }, // Disable sorting for the first column
+                { "title": "SKU" },
+                { "title": "Location" },
+                { "title": "Unit" },
+                { "title": "Unit Qty" },
+                { "title": "Min Qty" },
+                { "title": "Refill Qty" },
+                { "title": "QOS" },
+                { "title": "Status" },
+                { "title": "Notes" }
+            ],
+            order: [], // Disable initial sorting
+            pageLength: 100, // Set default number of entries per page to 100
+            columnDefs: [
+                { targets: [0, 1, 2, 3, 4, 5, 6, 7, 8], className: 'text-nowrap' }, // Apply 'text-nowrap' class to columns 0 to 8
+                {
+                    targets: 0, // Target the Product Name column
+                    render: function (data, type, row, meta) {
+                        if (type === 'display') {
+                            var originalProductName = data;
+                            var productName = originalProductName;
 
-        // Truncate productName to 67 characters including spaces
-        if (productName.length > 67) {
-            productName = productName.substring(0, 67) + '...';
-        }
+                            // Truncate productName to 67 characters including spaces
+                            if (productName.length > 67) {
+                                productName = productName.substring(0, 67) + '...';
+                            }
 
-        // Find the matching record in the JSON data using originalProductName
-        var matchingRecord = jsonData.rows.find(record => record.cell[4] === originalProductName);
-        if (matchingRecord) {
-     // Use truncated productName for strippedProductName
-            var strippedProductName = productName.replace(/<[^>]*>/g, ''); // Remove HTML tags from productName
-            // Retrieve the URL from the matching record
-            var imageUrl = matchingRecord.cell[2];
-            // Prepend "https://images.printable.com" to the URL
-            var fullImageUrl = "https://images.printable.com" + imageUrl;
-            // Create the link with the productName and attach onclick event
-            var productNameLink = '<a href="' + fullImageUrl + '" target="_blank">' + strippedProductName + '</a>';
-            
-            // Create the link for the "Refill" icon image to trigger the sendRefillEmail function
-            var refillLink = '<a href="#" onclick="sendRefillEmail(\'' + strippedProductName + '\'); return false;"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i></a>';
-          // Create the link for the "Location Change" icon image to trigger the sendLocationEmail function
-            var locationLink = '<a href="#" onclick="sendLocationEmail(\'' + strippedProductName + '\'); return false;"><i class="fa fa-map-marker" aria-hidden="true"></i></a>';
-            // Return the combined content of productName link and refillLink
-            return productNameLink + ' ' + refillLink + ' ' + locationLink;
-        } else {
-            // If no match found, return the truncated Product Name
-            return productName;
-        }
-    } else {
-        // Return original data for other types (sorting, filtering, etc.)
-        return data;
+                            // Find the matching record in the JSON data using originalProductName
+                            var matchingRecord = jsonData.rows.find(record => record.cell[4] === originalProductName);
+                            if (matchingRecord) {
+                                // Use truncated productName for strippedProductName
+                                var strippedProductName = productName.replace(/<[^>]*>/g, ''); // Remove HTML tags from productName
+                                // Retrieve the URL from the matching record
+                                var imageUrl = matchingRecord.cell[2];
+                                // Prepend "https://images.printable.com" to the URL
+                                var fullImageUrl = "https://images.printable.com" + imageUrl;
+                                // Create the link with the productName and attach onclick event
+                                var productNameLink = '<a href="' + fullImageUrl + '" target="_blank">' + strippedProductName + '</a>';
+
+                                // Create the link for the "Refill" icon image to trigger the sendRefillEmail function
+                                var refillLink = '<a href="#" onclick="sendRefillEmail(\'' + strippedProductName + '\'); return false;"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i></a>';
+                                // Create the link for the "Location Change" icon image to trigger the sendLocationEmail function
+                                var locationLink = '<a href="#" onclick="sendLocationEmail(\'' + strippedProductName + '\'); return false;"><i class="fa fa-map-marker" aria-hidden="true"></i></a>';
+                                // Return the combined content of productName link and refillLink
+                                return productNameLink + ' ' + refillLink + ' ' + locationLink;
+
+                            } else {
+                                // If no match found, return the truncated Product Name
+                                return productName;
+                            }
+                        } else {
+                            // Return original data for other types (sorting, filtering, etc.)
+                            return data;
+                        }
+                    }
+                },
+                {
+                    targets: 1, // Target the SKU column
+                    render: function (data, type, row, meta) {
+                        if (type === 'display') {
+                            // Convert SKU value to a clickable link opening in a new tab
+                            return '<a href="https://marcomcentral.app.pti.com/westpress/WestPressInventory/catalogsearch.aspx?uigroup_id=1677&query=' + encodeURIComponent(data) + '" target="_blank">' + data + '</a>';
+                        } else {
+                            return data; // Return original data for other types (sorting, filtering, etc.)
+                        }
+                    }
+                }
+            ]
+        });
+
+        // Iterate over each row in the table
+        table.rows().every(function (rowIdx, tableLoop, rowLoop) {
+            var rowData = this.data();
+            var status = rowData[8]; // Assuming "Status" column is at index 7
+
+            // Apply styling based on the value of the "Status" column
+            if (status === "BACKORDER") {
+                $(this.node()).addClass('backorder');
+            } else if (status === "BELOW OR AT SET MINIMUM") {
+                $(this.node()).addClass('below-minimum');
             }
-          }
-        },
-        {
-          targets: 1, // Target the SKU column
-          render: function (data, type, row, meta) {
-            if (type === 'display') {
-              // Convert SKU value to a clickable link opening in a new tab
-              return '<a href="https://marcomcentral.app.pti.com/westpress/WestPressInventory/catalogsearch.aspx?uigroup_id=1677&query=' + encodeURIComponent(data) + '" target="_blank">' + data + '</a>';
-            } else {
-              return data; // Return original data for other types (sorting, filtering, etc.)
-            }
-          }
-        }
-      ]
+        });
+    }).catch(error => {
+        console.error('Error rendering DataTable:', error);
     });
-  
-    // Iterate over each row in the table
-    table.rows().every(function (rowIdx, tableLoop, rowLoop) {
-      var rowData = this.data();
-      var status = rowData[8]; // Assuming "Status" column is at index 7
-  
-      // Apply styling based on the value of the "Status" column
-      if (status === "BACKORDER") {
-        $(this.node()).addClass('backorder');
-      } else if (status === "BELOW OR AT SET MINIMUM") {
-        $(this.node()).addClass('below-minimum');
-      }
-    });
-  }).catch(error => {
-    console.error('Error rendering DataTable:', error);
-  });
 }
-
-
 
 // Get the last time the spreadsheet was modified
 
@@ -133,7 +130,7 @@ function getLastModifiedTime() {
             return null;
         });
 }
-function sendEmail(productName) {
+function sendRefillEmail(productName) {
     // Extract SKU from productName
     var sku = extractSkuFromProductName(productName);
 
@@ -158,6 +155,7 @@ function sendEmail(productName) {
     // Open the email client
     window.location.href = emailLink;
 }
+
 function sendLocationEmail(productName) {
     // Extract SKU from productName
     var sku = extractSkuFromProductName(productName);
