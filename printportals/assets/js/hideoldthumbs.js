8f2fd8ed5a77a1cce413a902e703d7b0bda83223
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2025-02-14 13:28:13 -0700

    Create hideoldthumbs.js

---
+++ printportals/assets/js/hideoldthumbs.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // List of image URLs to hide
+    const blockedImages = [
+        "https://images.printable.com/printonelogos/images/895/5913/folders/2956766/de665160-b21b-4859-88c2-cd85d0131f40.png",
+        "https://images.printable.com/some-other-image.jpg"
+    ];
+
+    document.querySelectorAll("img").forEach(img => {
+        if (blockedImages.includes(img.src)) {
+            img.style.display = "none"; // Hide the image
+            img.alt = ""; // Remove alt text
+        }
+    });
+});
