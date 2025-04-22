375ecf718767d48adddf0900b0b94c01cd43faf4
wp-marcom <113721322+wp-marcom@users.noreply.github.com>
2025-04-22 07:10:03 -0700

    Update login.js

--- inventorytrackers/portalsync/assets/js/login.js
+++ inventorytrackers/portalsync/assets/js/login.js
@@ -14,6 +14,7 @@ const users = [
   { username: "east1", password: "east1", role: "warehouse", fullName: "East Warehouse 1" },
   { username: "east2", password: "east2", role: "warehouse", fullName: "East Warehouse 2" },
   { username: "linda", password: "Charlie1", role: "lprice", fullName: "Linda" },
+  { username: "dave", password: "dave", role: "lprice", fullName: "Dave" },
   { username: "acharles", password: "acharles", role: "acharles", fullName: "Andy" },
   { username: "molson", password: "molson", role: "molson", fullName: "Marlo" }
 ];
